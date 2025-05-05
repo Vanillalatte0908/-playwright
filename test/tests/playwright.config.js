5f652b2ce9561ecfa430b7036e947fbbe281b315
Vanillalatte <junkmailist0908@gmail.com>
2025-05-05 15:56:51 +0700

    full end to end

--- test/tests/playwright.config.js
+++ test/tests/playwright.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-    // Specify the path to your custom fixtures.js file
-    testDir: './tests', // Directory where your tests are located
-    // Optional: Setup for the browser (if needed)
-    use: {
-      baseURL: 'http://borobudur-svc.linkaja.dev:8000', // If you have a common base URL for API requests
-    },
-    // Other configurations (optional)
-  };
-  
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+        // Specify the path to your custom fixtures.js file
+         testDir: './tests', // Directory where your tests are located
+        // Optional: Setup for the browser (if needed
+        use: {
+          headless: false, // 👈 Show the browser
+          viewport: { width: 1280, height: 720 },
+          // slowMo: 500, // Optional: slow down actions to see them clearly
+        }
+      });
